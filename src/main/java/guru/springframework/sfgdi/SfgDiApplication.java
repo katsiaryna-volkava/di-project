506a787ff0766123b8ec6de1d2bdfcefa99aa294
EkaterinaVolkova <e.volkova@syberry.com>
2020-09-26 23:12:49 +0300

    adding initial app

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) context.getBean("myController");
+		String greetings = myController.sayHello();
+		System.out.println(greetings);
+	}
+
+}
