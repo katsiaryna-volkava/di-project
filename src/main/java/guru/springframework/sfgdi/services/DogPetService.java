87b491bb32ce86434b8acf90003b7c4a3d8d5e34
EkaterinaVolkova <e.volkova@syberry.com>
2020-09-27 01:34:51 +0300

    fixing pets issues

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,12 +1,16 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Primary
 @Profile({"dog", "default"})
-public class DogPetService {
+@Service
+public class DogPetService implements PetService{
     public String getPetType(){
         return "Dogs are the best!";
     }
--- src/main/java/guru/springframework/sfgdi/services/PetService.java
+++ src/main/java/guru/springframework/sfgdi/services/PetService.java
@@ -4,6 +4,5 @@ package guru.springframework.sfgdi.services;
  * Created by jt on 12/28/19.
  */
 public interface PetService {
-
     String getPetType();
 }
